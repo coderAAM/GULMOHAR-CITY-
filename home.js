1bd0b030fdec2d2d59aa66a4c73f6367d4d1f334
AHMED ALI MUGHAL <163463244+coderAAM@users.noreply.github.com>
2025-06-21 22:51:17 -0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('mainBtn').onclick = function() {
+        window.location.href = "../links.html";
+    };
+    document.getElementById('backBtn').onclick = function() {
+        window.location.href = "../welcome.html";
+    };
+});
